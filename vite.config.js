f54219167ff42fe9ff555a6ccc357b5f6c7fc524
Marshelinda Rukmana <marshelinda03@gmail.com>
2026-07-13 23:22:48 +0700

    fix: merge allowedHosts with existing proxy in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     // 1. Izinkan domain HTTPS kamu agar tidak terkena "Blocked request"
+    host: true,
     allowedHosts: [
       "task-manager.marshelinda.my.id"
     ],
